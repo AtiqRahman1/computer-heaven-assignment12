89f038549b0c93f1235aee93e97d01684a54ea88
AtiqRahman1 <atiq94.ar@gmail.com>
2022-05-29 00:46:00 +0600

    add review page update

--- src/components/Pages/Dashboard/AddReview.js
+++ src/components/Pages/Dashboard/AddReview.js
@@ -3,7 +3,16 @@ import React from 'react';
 const AddReview = () => {
     return (
         <div>
-            <h2>add a review</h2>
+            <h2 className='text-2xl text-center mt-5'>Add Your Review</h2>
+            <div className=' flex h-screen justify-center '>
+                <form >
+                    <textarea class="textarea w-80 textarea-bordered" placeholder="Your opinion"></textarea>
+
+                    <input type="text" placeholder="Ratings" class="input input-bordered w-full max-w-xs" />
+                    <br />
+                    <input className='btn btn-accent' type="submit" value="Add" />
+                </form>
+            </div>
         </div>
     );
 };
--- src/components/Pages/Home/Reviews.js
+++ src/components/Pages/Home/Reviews.js
@@ -11,12 +11,12 @@ const Reviews = () => {
 
     return (
         <div className='mt-10'>
-            <h2 className='text-3xl text-purple-500 font-bold'>Reviews</h2>
+            <h2 className='text-3xl text-orange-400 font-bold'>Reviews</h2>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
                 {
                     reviews.map(review => <div
                         key={review._id}
-                        className="card w-96 bg-purple-400 text-white shadow-xl">
+                        className="card w-96 bg-orange-200 text-white shadow-xl">
                         <div className="card-body items-center text-center">
                             <h2 className="card-title">{review.name}</h2>
                             <p>Feedback: {review.message}</p>
